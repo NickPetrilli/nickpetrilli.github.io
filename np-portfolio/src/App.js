cbbb5eaff2884720b5769bc77d94dfceddb41d92
NickPetrilli <Nicholas.Petrilli1@marist.edu>
2024-07-24 14:52:46 -0400

    Creation of react app

---
+++ np-portfolio/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Welcome to My Portfolio</h1>
+        <p>Hello! I'm Nick Petrilli, a Software Developer.</p>
+      </header>
+
+      <footer>
+        <p>&copy; 2024 Nick Petrilli. All rights reserved.</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
