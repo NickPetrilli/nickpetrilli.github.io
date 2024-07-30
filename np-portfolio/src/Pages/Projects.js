0e4685337b655c4a44ae0ea68c30c736f8089193
NickPetrilli <Nicholas.Petrilli1@marist.edu>
2024-07-30 14:40:00 -0400

    Created and linked all FoxLift demo vids

--- np-portfolio/src/Pages/Projects.js
+++ np-portfolio/src/Pages/Projects.js
@@ -26,7 +26,7 @@ const projects = [
         title: 'FoxLift',
         description: 'A mobile ride sharing application for students that uses Google Authentication and the Google Maps API.',
         imageUrl: '/images/FoxLift.png',
-        demoUrl: '/videos/NewUserDemo.mp4',
+        demoUrl: '/videos/FoxLiftDemos.mp4',
         sourceCodeUrl: 'https://github.com/NickPetrilli/RideSharingApp',
         languages: ['React Native', 'JavaScript', 'MySQL', 'Linux'],
         longDescription: ''
