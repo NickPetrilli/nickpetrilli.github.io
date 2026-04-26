5dead121962c1f92044dafeb99dab649797f3a04
NickPetrilli <Nicholas.Petrilli1@marist.edu>
2026-04-25 22:11:55 -0400

    fix resume sizing

--- np-portfolio/src/Pages/Resume.js
+++ np-portfolio/src/Pages/Resume.js
@@ -1,22 +1,16 @@
 import React from 'react';
 
 const Resume = () => {
-    return (
-
-      <div 
-      id="divMain" 
-      style={{ width: '100%', height: 'auto' }} 
-      className="scroll"
-  >
-      <iframe 
-          src="/documents/NP_Resume_2024.pdf" 
-          width="100%" 
-          height="800px"
-          title="Resume"
-      ></iframe>
-  </div>
-
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', flex: 1, minHeight: 'calc(100vh - 60px)' }}>
+      <iframe
+        src="/documents/NP_Resume_2024.pdf"
+        width="100%"
+        style={{ flex: 1, border: 'none', minHeight: 'calc(100vh - 60px)' }}
+        title="Resume"
+      />
+    </div>
     );
-  };
+  };  
 
 export default Resume;
